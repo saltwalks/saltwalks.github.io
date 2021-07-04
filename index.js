fab85b54736a115f75e302d8e7622c9864eb656b
saltwalks <ewqdsacxz2345@gmail.com>
2021-07-05 00:56:35 +0900

    Done : Personal Information Modal

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Main Slider
+/*------------------------------ Main Slider ---------------------------------*/
 const slideList = document.getElementById("slide-list");
 const imageNum = slideList.children.length;
 slideList.style.width = window.innerWidth * imageNum + "px";
@@ -9,3 +9,37 @@ setInterval(() => {
     cnt = cnt < imageNum - 1 ? cnt + 1 : 0;
     slideList.style.transform = `translateX(-${window.innerWidth * cnt}px)`;
 }, 5000);
+
+/*------------------------------- Modal -------------------------------------*/
+const boring = document.getElementById("boring");
+const modalOverlay = document.querySelector(".modal-overlay");
+
+boring.addEventListener("click", () => {
+    modalOverlay.style.display = "flex";
+});
+
+modalOverlay.addEventListener("click", event => {
+    const overlayOnly = event.target;
+    if(overlayOnly.classList.contains("modal-overlay")) {
+        modalOverlay.style.display = "none";
+    }
+});
+
+/*------------------------------- Copy -------------------------------------*/
+function clipboardCopy(id) {
+    const textToCopy = document.getElementById(id.replace("-copy", ""));
+    navigator.clipboard.writeText(textToCopy.innerHTML).then(() => {
+        alert("Clipboard Copied!");
+    }).catch(() => {
+        alert("Damn");
+    });
+}
+
+/*------------------------------- Resume -------------------------------------*/
+function getResume(id) {
+    if(id.replace("-resume", "") === "en") {
+        alert("English Resume Not Prepared");
+    } else {
+        alert("Korean Resume Not Prepared");
+    }
+}
