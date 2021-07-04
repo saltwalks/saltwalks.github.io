645d0d5bebb86e0e81cf2fe2a13417d5bd4e87d0
saltwalks <ewqdsacxz2345@gmail.com>
2021-07-04 16:53:30 +0900

    Done : Thoth Introduction Page

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const windowWidth = window.innerWidth;
-
 // Main Slider
 const slideList = document.getElementById("slide-list");
 const imageNum = slideList.children.length;
-slideList.style.width = windowWidth * imageNum + "px";
+slideList.style.width = window.innerWidth * imageNum + "px";
 
 let cnt = 0;
 setInterval(() => {
     slideList.style.transition = "500ms";
     cnt = cnt < imageNum - 1 ? cnt + 1 : 0;
-    slideList.style.transform = `translateX(-${windowWidth * cnt}px)`;
+    slideList.style.transform = `translateX(-${window.innerWidth * cnt}px)`;
 }, 5000);
